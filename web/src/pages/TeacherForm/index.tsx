120e83ed0772f7141c7cacbb4540ced22f02be5d
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-08-05 11:26:12 -0300

    Finaliza a pagina de aulas e adiciona mais componentes

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,64 @@
 import React from "react";
-import PageHeader from "../../components/page-header";
+import PageHeader from "../../components/PageHeader";
+import './styles.css';
+import Input from "../../components/Input";
+import warningIcon from '../../assets/images/icons/warning.svg';
+import Textarea from "../../components/Textarea";
+import Select from './../../components/Select/index';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
+      
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscrição."
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome Completo"/>
+          <Input name="avatar" label="Avatar"/>
+          <Input name="whatsapp" label="WhatsApp"/>
+          <Textarea name="bio" label="Biografia"/>
+        </fieldset>
+
+        
+        <fieldset>
+          <legend>Sobre a Aula</legend>
+
+          <Select
+           name="subject" 
+           label="Matéria"
+           options={[
+             {value: 'Artes', label: 'Artes'},
+             {value: 'Biologia', label: 'Biologia'},
+             {value: 'Educação Física', label: 'Educação Física'},
+             {value: 'Física', label: 'Física'},
+             {value: 'Geografia', label: 'Geografia'},
+             {value: 'História', label: 'História'},
+             {value: 'Matemática', label: 'Matemática'},
+             {value: 'Português', label: 'Português'},
+             {value: 'Química', label: 'Química'},
+           ]}
+           />
+          <Input name="cost" label="Custo da sua hora por aula"/>         
+        </fieldset>
+
+
+      <footer>
+        <p><img src={warningIcon} alt="Aviso Importante"/>
+        Importante: <br/>
+        Preencha seus dados
+        </p>
+
+        <button type="button">Salvar Cadastro</button>
+      </footer>
+      </main>
     </div>
+    
   );
 }
 
--- web/src/pages/TeachersList/index.tsx
+++ web/src/pages/TeachersList/index.tsx
@@ -1,29 +1,21 @@
 import React from "react";
-
-import PageHeader from "../../components/page-header";
+import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
+import Input from './../../components/Input/index';
 
 import "./styles.css";
-import TeacherItem from "../../components/TeacherItem";
+
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
 
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da Semana</label>
-            <input type="text" id="week-day" />
-          </div>
+          <Input name="subject" label="Matéria"/>
+          <Input name="week-day" label="Dia da semana"/>
+          <Input name="time" label="Hora" type="time"/>
 
-          <div className="input-block">
-            <label htmlFor="hour">Hora</label>
-            <input type="text" id="hour" />
-          </div>
         </form>
       </PageHeader>
 
